e2a359cf48b554eec48e6d2be49e48f84d7a2964
Sungwoo Park <codest99@gmail.com>
2021-02-24 16:43:55 +0900

    Finish!!!

--- srcs/wordpress/srcs/start.sh
+++ srcs/wordpress/srcs/start.sh
@@ -5,6 +5,7 @@ rm -rf custom-wp.tar.gz
 mv /custom-wp/ /var/www/wp
 chmod -R 755 /var/www/*
 
+sleep 10
 mysql -hmysql -Dwordpress -uroot -ppasswd < /wordpress.sql
 
 php-fpm7 & nginx -g "daemon off;"
